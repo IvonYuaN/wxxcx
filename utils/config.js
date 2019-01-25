1d73c605eb9e47be5ba8d7bfb951f09d71860c3f
Weyooz <50575415@qq.com>
2019-01-25 17:32:41 +0800

    Add files via upload

--- utils/config.js
+++ utils/config.js
@@ -1,18 +1,16 @@
 /*
  * 
  * WordPres版微信小程序
- * author: jianbo
- * organization: 守望轩  www.watch-life.net
- * github:    https://github.com/iamxjb/winxin-app-watch-life.net
- * 技术支持微信号：iamxjb
- * 开源协议：MIT
- * Copyright (c) 2017 https://www.watch-life.net All rights reserved.
+ * author: Weyooz
+ * organization: 未由时光  weyooz.cn
+ * github:    https://github.com/Weyooz/wxxcx
+ * Copyright (c) 2019 https://weyooz.cn All Rights Reserved.
+ * 
  */
 
 
-
 //配置域名,域名只修改此处。
-//如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
+//如果wordpress没有安装在网站根目录请加上目录路径,例如："weyooz.cn/blog"
 var DOMAIN = "weyooz.cn";
 var MINAPPTYPE="1";//小程序的类型，如果是企业小程序请填：0 ，如果是个人小程序请填：1
 var WEBSITENAME="未由时光"; //网站名称
@@ -24,9 +22,9 @@ var INDEXLISTTYPE="all"; //首页显示所有分类
 //var INDEXLISTTYPE ="1" //指定首页显示分类的id
 var PAYTEMPPLATEID = 'EiHxYpEt3PdsiLYNWlPfSRXl8gP7hc9FcVvNKP76DLQ';//鼓励消息模版id
 var REPLAYTEMPPLATEID = 'lh_Cd4vOhRK6FdaBmQsM77qc-P0Pgv_o487-GvqxhWw';//回复评论消息模版id
-var ZANIMAGEURL = 'https://weyooz.cn/wp-content/uploads/xcx/wxzs.png';//微信鼓励的图片链接，用于个人小程序的赞赏
+var ZANIMAGEURL = 'https://cdn.weyooz.cn/wp-content/uploads/xcx/wxzs.png';//微信鼓励的图片链接，用于个人小程序的赞赏
 var LOGO = "../../images/icon.png"; // 网站的logo图片
-var POSTERIMGURL ="../../images/logo2.png"; //生成海报如果没有首图，使用此处设置的图片作为海报图片。
+var POSTERIMGURL ="../../images/website.png"; //生成海报如果没有首图，使用此处设置的图片作为海报图片。
 //设置downloadFile合法域名,不带https ,在中括号([])里增加域名，格式：{id=**,domain:'www.**.com'}，用英文逗号分隔。
 //此处设置的域名和小程序与小程序后台设置的downloadFile合法域名要一致。
 var DOWNLOADFILEDOMAIN = [
@@ -39,12 +37,11 @@ var DOWNLOADFILEDOMAIN = [
  //        'appid' 当redirecttype为miniapp时，这个值为其他微信小程序的appid，如果redirecttype为apppage，webpage时，这个值设置为空。
  //         'extraData'当redirecttype为miniapp时，这个值为提交到其他微信小程序的参数，如果redirecttype为apppage，webpage时，这个值设置为空。
 var INDEXNAV = [
-  { id: '1', name: '微慕', image: '../../images/nav-icon1.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wx074a216a7aabc30c', extraData: '' },
-  { id: '2', name: '企业版', image: '../../images/nav-icon2.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxc1771b619b83316b', extraData: '' },
-  { id: '3', name: '旅游版', image: '../../images/nav-icon3.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxa43aaa8416cdbd61', extraData: '' },
-  { id: '4', name: '图片版', image: '../../images/nav-icon4.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxe9440e3d24b04152', extraData: ''},
+  { id: '1', name: '未由', image: '../../images/nav-icon1.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wx074a216a7aabc30c', extraData: '' },
+  { id: '2', name: '企业', image: '../../images/nav-icon2.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxc1771b619b83316b', extraData: '' },
+  { id: '3', name: '旅游', image: '../../images/nav-icon3.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxa43aaa8416cdbd61', extraData: '' },
+  { id: '4', name: '图片', image: '../../images/nav-icon4.png', url: 'pages/index/index', redirecttype: 'miniapp', appid: 'wxe9440e3d24b04152', extraData: ''},
   { id: '5', name: '微店', image: '../../images/nav-icon5.png', url: 'pages/shelf/shelf', redirecttype: 'miniapp', appid: 'wx55ea6098e41af5c4', extraData: ''}
-  // { id: '6', name: '教程', image: '../../images/app.png', url: '../../pages/list/list?categoryID=1059', redirecttype: 'apppage', appid: '', extraData: ''}
     
     ];
 
